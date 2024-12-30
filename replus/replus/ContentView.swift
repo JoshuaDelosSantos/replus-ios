9ed242c1cc15cc6c5b6ba247e50b2bacb61126b5
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-30 19:46:50 +1000

    Initial commit: Add replus swiftui project

---
+++ replus/replus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  replus
+//
+//  Created by Joshua Delos Santos on 30/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ replus/replus/replusApp.swift
@@ -0,0 +1,17 @@
+//
+//  replusApp.swift
+//  replus
+//
+//  Created by Joshua Delos Santos on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct replusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
