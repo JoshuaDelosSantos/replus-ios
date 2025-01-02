a1679ce3b7cbcfb06290d891e5423f3054ff8d95
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 09:53:39 +1000

    Add initial structure in Exercise model

--- replus/replus/Models/Exercise.swift
+++ replus/replus/Models/Exercise.swift
@@ -1,7 +1,21 @@
-//
-//  Exercise.swift
-//  replus
-//
-//  Created by Joshua Delos Santos on 3/1/2025.
-//
+/*
+ Exercise.swift
+ 
+ Joshua Delos Santos
+ 03/01/2025
+ 
+ */
 
+
+import Foundation
+
+
+struct Exercise: Identifiable {
+    let id: UUID
+    var name: String
+    
+    init(id: UUID = UUID(), name: String) {
+        self.id = id
+        self.name = name
+    }
+}
