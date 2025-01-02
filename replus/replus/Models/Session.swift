8e99daa6769d4fc5f18bece4781c48c49af58933
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-02 19:00:18 +1000

    Initial commit

---
+++ replus/replus/Models/Session.swift
@@ -0,0 +1,14 @@
+//
+//  Session.swift
+//  replus
+//
+//  Created by Joshua Delos Santos on 2/1/2025.
+//
+
+
+import Foundation
+
+struct Session: Identifiable {
+    let id = UUID()
+    var name: String
+}
