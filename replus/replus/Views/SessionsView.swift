b4fe9c7186e1217a7e455a8d672feed75ede2956
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-02 19:22:14 +1000

    Add initial view code to display sampleSessions

--- replus/replus/Views/SessionsView.swift
+++ replus/replus/Views/SessionsView.swift
@@ -10,3 +10,22 @@
 import SwiftUI
 
 
+struct SessionsView: View {
+    let sessions = Session.sampleSessions
+    
+    var body: some View {
+        NavigationView {
+            List(sessions) { session in
+                Text(session.name)
+            }
+            .navigationTitle("Sessions")
+        }
+    }
+}
+
+
+struct SessionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SessionsView()
+    }
+}
