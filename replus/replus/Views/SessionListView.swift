7f7e277f2fc6485336d606ca74c0368ce66781a3
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-02 19:14:47 +1000

    Rename file from SessionListView to SessionsView

--- replus/replus/Views/SessionListView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SessionListView.swift
-//  replus
-//
-//  Created by Joshua Delos Santos on 2/1/2025.
-//
-
---
+++ replus/replus/Views/SessionsView.swift
@@ -0,0 +1,12 @@
+/*
+ SessionListView.swift
+ 
+ Joshua Delos Santos
+ 02/01/2025
+ 
+ */
+
+
+import SwiftUI
+
+
