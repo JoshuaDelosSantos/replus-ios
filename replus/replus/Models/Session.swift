1f2151b0370286eaa517448de4f8f59b8a70750d
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 10:05:42 +1000

    Update model to use Exercise sample data

--- replus/replus/Models/Session.swift
+++ replus/replus/Models/Session.swift
@@ -13,11 +13,13 @@ import Foundation
 struct Session: Identifiable {
     let id: UUID
     var name: String
+    var exercises: [Exercise]
     
-    
-    init(id: UUID = UUID(), name: String) {
+    // Currently initialised using sample data.
+    init(id: UUID = UUID(), name: String, exercises: [Exercise] = Exercise.sampleExercises) {
         self.id = id
         self.name = name
+        self.exercises = exercises
     }
 }
 
