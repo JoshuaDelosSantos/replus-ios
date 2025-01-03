b672284ec7d12e306ee5aa94f102c2611082e759
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 12:47:58 +1000

    Add initial code

--- replus/replus/CoreData/PersistenceController.swift
+++ replus/replus/CoreData/PersistenceController.swift
@@ -6,3 +6,22 @@
  
  */
 
+
+import CoreData
+
+
+class PersistenceController {
+    static let shared = PersistenceController()
+    
+    private let coreDataStack = CoreDataStack.shared
+    
+    func saveContext() {
+        if coreDataStack.viewContext.hasChanges {
+            do {
+                try coreDataStack.viewContext.save()
+            } catch {
+                print("Error saving context: \(error)")
+            }
+        }
+    }
+}
