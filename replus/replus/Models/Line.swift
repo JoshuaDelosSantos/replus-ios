38b246b76c49177573423e68cba4a4d80839bad8
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 10:33:40 +1000

    Add initial structure of the Line model

--- replus/replus/Models/Line.swift
+++ replus/replus/Models/Line.swift
@@ -6,3 +6,20 @@
  
  */
 
+
+import Foundation
+
+
+struct Line: Identifiable {
+    let id: UUID
+    var reps: Int
+    var weight: Double
+    let date: Date
+    
+    init(id: UUID = UUID(), reps: Int, weight: Double, date: Date) {
+        self.id = id
+        self.reps = reps
+        self.weight = weight
+        self.date = date
+    }
+}
