c275178928abb25e408e393e1cc045efc22458c3
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 10:43:14 +1000

    Add helper function to auto-generate date stamp

--- replus/replus/Models/Line.swift
+++ replus/replus/Models/Line.swift
@@ -14,12 +14,31 @@ struct Line: Identifiable {
     let id: UUID
     var reps: Int
     var weight: Double
-    let date: Date
+    let date: String
     
-    init(id: UUID = UUID(), reps: Int, weight: Double, date: Date) {
+    init(id: UUID = UUID(), reps: Int, weight: Double, date: String = Line.generateDate()) {
         self.id = id
         self.reps = reps
         self.weight = weight
         self.date = date
+    }
+    
+    /// Helper function to generate a formatted date.
+    private static func generateDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yy"  // Set the desired date format
+        return formatter.string(from: Date()) // Use the current date
+    }
+}
+
+
+extension Line {
+    static var sampleLines: [Line] {
+        [
+            // Dates are auto-generated.
+            Line(reps: 10, weight: 50.0),
+            Line(reps: 8, weight: 60.5),
+            Line(reps: 12, weight: 45.0)
+        ]
     }
 }
